48fdf3c51b74fafe0036ec2cd9cb60532190a57f
SergioCuervoL <scuervoleon@uniminuto.edu.co>
2020-03-13 23:57:47 -0500

    Se insertan nuevas modificaciones al frontend

--- WEB/js/metodos.js
+++ WEB/js/metodos.js
@@ -9,5 +9,11 @@ $(document).ready(function(){
     $('#exampleRadios2').click(function(){
         $('#datosRestaurante').hide();
     });
+    $('#plus').click(function(){
+        $("<br> <div class='col mr-auto ml-auto text-center' id='ContenidoProducto'> <div class='row'> <div class='col mr-auto ml-auto text-center'> <input type='text' id='nombre' class='form-control' placeholder='Nombre' required autofocus> </div> <div class='col mr-auto ml-auto text-center'> <input type='text' id='cantidad' class='form-control' placeholder='Cantidad' required autofocus></div></div></div> <br>").insertAfter("#ContenidoProducto");
+    });
+    $('#añadirProducto').click(function(){
+        $('#registryDoc').show();
+    });
 });  
 
