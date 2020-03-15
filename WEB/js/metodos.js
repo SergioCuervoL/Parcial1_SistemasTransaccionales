8ef1c67c9127343c7181767972dc1f8ef24f22a6
SergioCuervoL <scuervoleon@uniminuto.edu.co>
2020-03-15 14:07:11 -0500

    Se ingresan las vistas faltantes con sus formularios correspondientes

--- WEB/js/metodos.js
+++ WEB/js/metodos.js
@@ -10,10 +10,39 @@ $(document).ready(function(){
         $('#datosRestaurante').hide();
     });
     $('#plus').click(function(){
-        $("<br> <div class='col mr-auto ml-auto text-center' id='ContenidoProducto'> <div class='row'> <div class='col mr-auto ml-auto text-center'> <input type='text' id='nombre' class='form-control' placeholder='Nombre' required autofocus> </div> <div class='col mr-auto ml-auto text-center'> <input type='text' id='cantidad' class='form-control' placeholder='Cantidad' required autofocus></div></div></div> <br>").insertAfter("#ContenidoProducto");
+        $("<tr id='#ContenidoProducto'><td><input type='text' id='nombreIngrediente' class='form-control' placeholder='Nombre' required autofocus></td> <td><input type='text id='cantidadIngrediente' class='form-control' placeholder='Cantidad' required autofocus></td><tr>").insertAfter("#ContenidoProducto");
     });
     $('#añadirProducto').click(function(){
         $('#registryDoc').show();
     });
+    $('#añadirMenu').click(function(){
+        $('#registryMenu').show();
+    });
+    $("#registrarProducto").click(function() {
+        var valores = "";
+				$("#nombreIngrediente").parent("tr").find("td").each(function() {
+        		if($(this).html() != ""){
+            	 valores += $(this).html() + " ";
+            }
+        });
+        valores = valores + "\n";
+        alert(valores);
+    });
 });  
 
+/*
+        var nFilas = $("#ingredientes tr").length;
+        var datos;
+        for (let i = 0; i < nFilas; i++) {
+            for (let j = 0; j < 2; j++) {
+                datos[i][j] = $("#ingredientes td").value;
+            }            
+        }
+        var res = "";
+        for (let i = 0; i < nFilas; i++) {
+            for (let j = 0; j < 2; j++) {
+                res = res + "" +datos[i][j] + "";
+            }            
+        }
+        alert(res);
+*/
