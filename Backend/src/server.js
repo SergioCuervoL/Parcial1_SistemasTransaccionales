8a529824f08be7ef3441f9fa2e88f0811a2c8b95
Alejandromendivelso <36489929+Alejandromendivelso@users.noreply.github.com>
2020-03-17 21:49:08 -0500

    Backend

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,28 @@
-const express = requiere('express');
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
+const myConnection = require('express-myconnection');
 const server = express(); 
 
-server.listen(3000, ()=> {
-    console.log('server in port 3000');
+// setting
+server.set('port', process.env.PORT || 3000);
+server.set('viw engine', 'ejs');
+server.set('views', path.join(__dirname, 'views'));
+
+ //middlewares
+server.use(morgan('dev'));
+server.use(myConnection(mysql, {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    port: 3306,
+    database: 'Parcial_1'
+}, 'single'));
+
+ // routes
+
+
+server.listen(server,get('port'), ()=> {
+    console.log('server n port 3000');
 });
